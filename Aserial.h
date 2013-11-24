89f6a8524c61a827795b55b810691de95f81d140
michael.blandford <michael.blandford@mypostoffice.co.uk>
2013-11-24 21:56:03 +0000

    ReWrite serial for SPort and Hub, PPM input on interrupt, sequence reading sensors

---
+++ Aserial.h
@@ -0,0 +1,36 @@
+/* ============================================================
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================*/
+
+// Author: Mike Blandford
+
+#ifdef FRSKY_SPORT
+struct t_sportData
+{
+	struct t_sportData *next ;
+	uint8_t data[7] ;
+	uint8_t dataLock ;
+	uint8_t serialSent ;
+} ;
+
+void setNewData( struct t_sportData *pdata, uint16_t id, uint32_t value ) ;
+void initSportUart( struct t_sportData *pdata ) ;
+
+extern uint8_t DataSent ;
+extern uint8_t SportSync ;
+
+#else // FRSKY_SPORT
+
+void initHubUart( void ) ;
+void sendHubByte( uint8_t byte ) ;
+
+#endif // FRSKY_SPORT
+
